1003d99b20e1db1c506ee33c47465fd900812c90
Coke And Code <kevin@rune.ai>
2024-11-28 12:19:09 +0000

    Add music and AI interaction

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,12 @@
 import "./styles.css"
 import { createPicker, darkTheme, InMemoryStoreFactory } from "picmo"
+import musicUrl from "./assets/music.mp3"
+
+const music = new Audio()
+music.src = musicUrl
+music.loop = true
+
+music.play()
 
 let started = false
 let currentScreen = "startScreen"
@@ -9,6 +16,8 @@ function div(id: string): HTMLDivElement {
 }
 
 document.body.addEventListener("click", () => {
+  music.play()
+
   if (!started) {
     started = true
     Rune.actions.start()
@@ -16,8 +25,9 @@ document.body.addEventListener("click", () => {
 })
 
 let pickerSelected = 1
+let winner = ""
 
-const selectedEmoji: string[] = ["", "", ""]
+let selectedEmoji: string[] = ["", "", ""]
 
 const picker = createPicker({
   rootElement: div("emojiPicker"),
@@ -29,7 +39,10 @@ const picker = createPicker({
 picker.addEventListener("emoji:select", (e) => {
   if (e.emoji.length > 0) {
     selectedEmoji[pickerSelected - 1] = e.emoji
-    Rune.actions.respond(selectedEmoji.join(""))
+    Rune.actions.respond({
+      name: localPlayerName,
+      emojis: selectedEmoji.join(""),
+    })
     div("emojiPickerBackground").style.display = "none"
   }
 })
@@ -66,12 +79,19 @@ function showScreen(screen: string) {
   }
 }
 
+let localPlayerName = ""
+
 Rune.initClient({
-  onChange: ({ game, event }) => {
+  onChange: ({ game, event, yourPlayerId }) => {
     if (event && event.name === "stateSync" && event.isNewGame) {
       started = false
       showScreen("startScreen")
     }
+
+    if (yourPlayerId) {
+      localPlayerName = Rune.getPlayerInfo(yourPlayerId).displayName
+    }
+
     started = game.started
 
     if (game.started) {
@@ -81,8 +101,50 @@ Rune.initClient({
 
       const remaining = game.timerEndsAt - Rune.gameTime()
       const percent = (Math.max(0, remaining) / game.timerTotalTime) * 100 + "%"
-      if (game.timerName === "question") {
-        div("questionTimerBar").style.width = percent
+      if (game.prompting) {
+        showScreen("thinkingScreen")
+      } else if (game.timerName === "question") {
+        if (currentScreen !== "questionScreen") {
+          selectedEmoji = ["", "", ""]
+          div("picker1").innerHTML = selectedEmoji[0]
+          div("picker2").innerHTML = selectedEmoji[1]
+          div("picker3").innerHTML = selectedEmoji[2]
+        }
+        if (yourPlayerId && game.questions[yourPlayerId]) {
+          div("questionTimerBar").style.width = percent
+          div("jobName").innerHTML = game.jobTitle
+          div("questionText").innerHTML =
+            game.questions[yourPlayerId][game.round]
+          showScreen("questionScreen")
+        }
+      } else if (game.timerName === "endgame") {
+        if (game.winner !== winner) {
+          winner = game.winner
+          // do the update for the winner
+          div("resultPanelTitle").innerHTML =
+            "🥳 " +
+            (Rune.getPlayerInfo(game.winner)?.displayName ?? "") +
+            " got the job!"
+          div("resultPanelContent").innerHTML = ""
+
+          for (let i = 0; i <= game.round; i++) {
+            const prompt = game.questions[game.winner]?.[i] ?? ""
+            const answer = game.emojis[game.winner]?.[i] ?? ""
+            const promptDiv = document.createElement("div")
+            promptDiv.innerHTML = prompt
+            div("resultPanelContent").appendChild(promptDiv)
+            promptDiv.classList.add("resultText")
+            const answerDiv = document.createElement("div")
+            answerDiv.innerHTML = answer
+            div("resultPanelContent").appendChild(answerDiv)
+            answerDiv.classList.add("resultEmojis")
+          }
+          const conclusionDiv = document.createElement("div")
+          conclusionDiv.innerHTML = game.conclusion
+          div("resultPanelContent").appendChild(conclusionDiv)
+          conclusionDiv.classList.add("resultText")
+        }
+        showScreen("resultScreen")
       }
     }
   },
--- src/logic.ts
+++ src/logic.ts
@@ -1,16 +1,53 @@
-import type { PlayerId, RuneClient } from "rune-sdk"
+import type { RuneClient } from "rune-sdk"
 
-export type Cells = (PlayerId | null)[]
+const QUESTION_TIMER = 20_000
+const END_TIMER = 60_000
+
+export type AiMessage = {
+  role: string
+  content: string
+}
+
 export interface GameState {
   emojis: Record<string, string[]>
   questions: Record<string, string[]>
+  playerNames: Record<string, string>
+  jobTitle: string
   winner: string
   round: number
   started: boolean
   timerTotalTime: number
   timerEndsAt: number
   timerName: string
+  prompting: boolean
+  messages: AiMessage[]
+  conclusion: string
 }
+
+const PROMPT =
+  "We are playing a game where you act an interviewer for crazy job. Players will respond using only emojis.\n" +
+  "You should generate a random crazy funny job. Output the job title on a line starting with Job Title:.\n" +
+  "You should then generate a short introduction to the interview for the job applicant on a line starting with Intro:.\n" +
+  "I will then provide emojis to answer on lines starting with Player <player name>:. You should not output any lines starting with Player:.\n" +
+  "When you receive the emojis you should generate a next step in the interview for each player on lines starting with Answer <player name>:\n" +
+  "After you have received 3 inputs from each player you should pick the best applicant based on their responses and output it on a line starting with Winner:\n" +
+  "You should also output a congratulations on getting the job message based on the last set of emojis on a line starting with Conclusion:\n" +
+  "\n" +
+  "Example with only 2 inputs:\n" +
+  "\n" +
+  "Job Title: Galactic Animal Whisperer\n" +
+  "Intro: Welcome to the interview! We’re hiring for a position with the Intergalactic Animal Conservation Society. This role requires connecting with rare and exotic creatures across the cosmos, soothing them, and understanding their needs.\n" +
+  "Player kev: 🦾☘️🥝\n" +
+  "Player coke and code: 👹🧖🫃\n" +
+  "Player shane helm: 🥝🍧👽\n" +
+  "Answer kev: Ah ha, I see you're strong and fruity. I wonder how that will play with the animals. Maybe you'll been bringing an extra zest. How would you approach difficult animals with your style?" +
+  "Answer coke and code:: Ah, I see a natural approach here! You have a refreshing calmness and a gentle presence, perfect for creatures with complex sensitivities. Tell me, how would you approach building trust with an alien species known for being cautious around new environments?" +
+  "Answer shane helm: Well there aren't many frozen aliens but I'm sure the you'll find something of interest. So, how would you handle the hot blooded animals in your care?" +
+  "Player kev: 🫶🧠👀\n" +
+  "Player coke and code: 🧑‍🦱💂‍♀️🕵️\n" +
+  "Player shane helm: 👳🏻‍♀️👨🏼‍🎨✌🏼\n" +
+  "Winner: kev\n" +
+  "Conclusion: Congratulations kev and welcome to our team. I'm sure you'll be a fantastic additional to the animal whispering team!"
 
 function startTimer(game: GameState, name: string, length: number) {
   game.timerName = name
@@ -19,7 +56,7 @@ function startTimer(game: GameState, name: string, length: number) {
 }
 
 type GameActions = {
-  respond: (emojis: string) => void
+  respond: (params: { name: string; emojis: string }) => void
   start: () => void
 }
 
@@ -40,18 +77,119 @@ Rune.initLogic({
       timerEndsAt: 0,
       timerTotalTime: 0,
       timerName: "",
+      prompting: false,
+      jobTitle: "",
+      messages: [],
+      playerNames: {},
+      conclusion: "",
+    }
+  },
+  ai: {
+    promptResponse: ({ response }, { game, allPlayerIds }) => {
+      game.prompting = false
+      game.messages.push({ role: "assistant", content: response })
+      const lines = response.split("\n")
+      for (const line of lines) {
+        if (line.startsWith("Job Title:")) {
+          game.jobTitle = line.substring(line.indexOf(":") + 1).trim()
+        }
+        if (line.startsWith("Intro:")) {
+          game.round = 0
+          for (const id of allPlayerIds) {
+            if (!game.questions[id]) {
+              game.questions[id] = []
+            }
+            game.questions[id][game.round] = line
+              .substring(line.indexOf(":") + 1)
+              .trim()
+          }
+          startTimer(game, "question", QUESTION_TIMER)
+        }
+        if (line.startsWith("Answer ")) {
+          const name = line.substring(line.indexOf(" ") + 1, line.indexOf(":"))
+          const answer = line.substring(line.indexOf(":") + 1).trim()
+
+          for (const id of allPlayerIds) {
+            if (
+              (game.playerNames[id]?.toLowerCase() ?? "") ===
+              name.toLowerCase().trim()
+            ) {
+              if (!game.questions[id]) {
+                game.questions[id] = []
+              }
+              game.questions[id][game.round] = answer
+            }
+          }
+          startTimer(game, "question", QUESTION_TIMER)
+        }
+        if (line.startsWith("Winner:")) {
+          const name = line.substring(line.indexOf(":") + 1).trim()
+
+          for (const id of allPlayerIds) {
+            if (
+              (game.playerNames[id]?.toLowerCase() ?? "") ===
+              name.toLowerCase().trim()
+            ) {
+              game.winner = id
+            }
+          }
+          startTimer(game, "endgame", END_TIMER)
+        }
+        if (line.startsWith("Conclusion:")) {
+          game.conclusion = line.substring(line.indexOf(":") + 1).trim()
+          startTimer(game, "endgame", QUESTION_TIMER)
+        }
+      }
+    },
+  },
+  updatesPerSecond: 10,
+  update: ({ game }) => {
+    if (game.timerEndsAt !== 0 && game.timerEndsAt < Rune.gameTime()) {
+      if (game.timerName === "question") {
+        game.timerEndsAt = 0
+
+        game.prompting = true
+        let inputs = ""
+        for (const id of Object.keys(game.emojis)) {
+          if (game.emojis[id]) {
+            if (game.emojis[id][game.round]) {
+              inputs +=
+                "Player " +
+                game.playerNames[id] +
+                ": " +
+                game.emojis[id][game.round] +
+                "\n"
+            } else {
+              inputs = "Player " + game.playerNames[id] + ": ❤️❤️❤️\n"
+            }
+          }
+        }
+        game.messages.push({ role: "system", content: inputs })
+        Rune.ai.promptRequest({
+          messages: JSON.parse(JSON.stringify(game.messages)),
+        })
+        game.round++
+      } else if (game.timerName === "endgame") {
+        Rune.gameOver()
+      }
     }
   },
   actions: {
     start: (_, { game }) => {
       if (!game.started) {
         game.started = true
+        game.prompting = true
+        game.messages.push({ role: "system", content: PROMPT })
+        Rune.ai.promptRequest({
+          messages: JSON.parse(JSON.stringify(game.messages)),
+        })
       }
     },
-    respond: (emojis, { game, playerId }) => {
+    respond: ({ name, emojis }, { game, playerId }) => {
       if (!game.emojis[playerId]) {
         game.emojis[playerId] = []
       }
+      game.playerNames[playerId] = name
       game.emojis[playerId][game.round] = emojis
     },
   },
